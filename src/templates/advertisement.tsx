0be5df35833ac456224c62b4d6c26574248ea4a7
nguyen.tuananh <nguyen.tuananh@petronas.com>
2024-05-08 13:23:00 +0700

    change banner

--- src/templates/advertisement.tsx
+++ src/templates/advertisement.tsx
@@ -6,7 +6,7 @@ interface AdvertisementProps {
 
 const Advertisement: React.FC<AdvertisementProps> = ({ logoBanners }) => {
   return (
-    <div className="logos bg-[#f05a24]">
+    <div className="logos bg-[#47b1c9]">
       <div className="logos-slide w-max px-4 py-2">
         {logoBanners?.map((imageUrl, index) => (
           <div key={index} className="px-4" style={{ display: 'inline' }}>
